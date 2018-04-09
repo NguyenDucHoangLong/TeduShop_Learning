5ebc0a54014fae510cc631d4dce47898692772af
DESKTOP-1ARVQ2M\LongNguyen <ndhlong1994@gmail.com>
2018-04-09 20:59:24 +0700

    repository and unit

--- TeduShop.Data/Repositories/ProductCategoryRepository.cs
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using TeduShop.Data.Infrastructure;
 using TeduShop.Model.Models;
-using System.Linq;
 
 namespace TeduShop.Data.Repositories
 {
-    public interface IProductCategoryRepository
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
     {
         IEnumerable<ProductCategory> GetByAlias(string alias);
     }
+
     public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
     {
-        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        public ProductCategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory)
         {
         }
 
